60c3f682e7a5d79033c0d3e507039cc11c8f754e
Jordi Sabat <jsabat@gmail.com>
2021-02-13 19:20:43 +0100

    ensure HttpAdapter calls post with Body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:meta/meta.dart';
@@ -11,12 +13,17 @@ class HttpAdapter {
   Future<void> request({
     @required String url,
     @required String method,
+    Map body,
   }) async {
     final headers = {
       'content-type': 'application/json',
       'accept': 'application/json',
     };
-    await client.post(url, headers: headers);
+    await client.post(
+      url,
+      headers: headers,
+      body: jsonEncode(body),
+    );
   }
 }
 
@@ -40,16 +47,20 @@ void main() {
       await sut.request(
         url: url,
         method: 'post',
+        body: {'any_key': 'any_value'},
       );
 
       // assert
-      verify(client.post(
-        url,
-        headers: {
-          'content-type': 'application/json',
-          'accept': 'application/json',
-        },
-      ));
+      verify(
+        client.post(
+          url,
+          headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json',
+          },
+          body: '{"any_key":"any_value"}',
+        ),
+      );
     });
   });
 }

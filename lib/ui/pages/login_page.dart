3837a4a43ce47e1afa5e00a9b00437779b2f9365
Jordi Sabat <jsabat@gmail.com>
2021-07-03 16:09:40 +0200

    add app icon

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -6,39 +6,74 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Container(
+              height: 240,
+              margin: EdgeInsets.only(bottom: 32),
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [
+                      Theme.of(context).primaryColorLight,
+                      Theme.of(context).primaryColorDark,
+                    ],
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(0, 0),
+                      spreadRadius: 0,
+                      blurRadius: 4,
+                      color: Colors.black,
+                    )
+                  ],
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(80),
+                  )),
               child: Image(
                 image: AssetImage('lib/ui/assets/logo.png'),
               ),
             ),
-            Text('Login'.toUpperCase()),
-            Form(
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      icon: Icon(Icons.email),
+            Text('Login'.toUpperCase(),
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headline1),
+            Padding(
+              padding: const EdgeInsets.all(32),
+              child: Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        icon: Icon(
+                          Icons.email,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
                     ),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      icon: Icon(Icons.lock),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8, bottom: 32),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          icon: Icon(Icons.lock),
+                        ),
+                        obscureText: true,
+                      ),
                     ),
-                    obscureText: true,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Enter'),
-                  ),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text('Create an Account'),
-                  )
-                ],
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Enter'),
+                    ),
+                    TextButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.person),
+                      label: Text('Create Account'),
+                    )
+                  ],
+                ),
               ),
             )
           ],

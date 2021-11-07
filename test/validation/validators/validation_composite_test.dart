f1cd47441070569cbc67b7ca4490cb41c2a63f50
Jordi Sabat <jsabat@gmail.com>
2021-11-07 19:58:39 +0100

    ensure ValidationComposite returns the first error found

--- test/validation/validators/validation_composite_test.dart
+++ test/validation/validators/validation_composite_test.dart
@@ -2,8 +2,8 @@ import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 import 'package:meta/meta.dart';
 
-import 'package:clean_flutter_app/presentation/protocols/protocols.dart';
 import 'package:clean_flutter_app/validation/protocols/protocols.dart';
+import 'package:clean_flutter_app/presentation/protocols/validation.dart';
 
 class ValidationComposite implements Validation {
   final List<FieldValidation> validations;
@@ -11,7 +11,14 @@ class ValidationComposite implements Validation {
   ValidationComposite(this.validations);
 
   String validate({@required String field, @required String value}) {
-    return null;
+    String error;
+    for (final validation in validations) {
+      error = validation.validate(value);
+      if (error?.isNotEmpty == true) {
+        return error;
+      }
+    }
+    return error;
   }
 }
 
@@ -48,7 +55,7 @@ void main() {
     when(validation3.field).thenReturn('other_field');
     mockValidation3(null);
 
-    sut = ValidationComposite([validation1, validation2]);
+    sut = ValidationComposite([validation1, validation2, validation3]);
   });
 
   test('Should return null if all validations returns null or empty', () {
@@ -56,5 +63,15 @@ void main() {
     final error = sut.validate(field: 'any_field', value: 'any_value');
 
     expect(error, null);
+  });
+
+  test('Should return the first error', () {
+    mockValidation1('error_1');
+    mockValidation2('error_2');
+    mockValidation3('error_3');
+
+    final error = sut.validate(field: 'any_field', value: 'any_value');
+
+    expect(error, 'error_1');
   });
 }

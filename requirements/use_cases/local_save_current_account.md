f68e3e297f75bf0aa8486704af637a8e23dae314
Jordi Sabat <jsabat@gmail.com>
2021-11-14 15:15:50 +0100

    ensure LocalSaveCurrentAccount calls SaveSecureCacheStorage with correct value

--- requirements/use_cases/local_save_current_account.md
+++ requirements/use_cases/local_save_current_account.md
@@ -1,7 +1,7 @@
 # Local Save Current Account Use Case
 
 > ## Success case
-1. System saves user access token in Cache securely
+1. ✅ System saves user access token in Cache securely
 
 > ## Exception - Failed to write to cache
 1. System returns an unexpected error message
---
+++ test/data/usecases/save_current_account/local_save_current_account_test.dart
@@ -0,0 +1,35 @@
+import 'package:clean_flutter_app/domain/entities/entities.dart';
+import 'package:clean_flutter_app/domain/usecases/usecases.dart';
+import 'package:faker/faker.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+import 'package:meta/meta.dart';
+
+class LocalSaveCurrentAccount implements SaveCurrentAccount {
+  final SaveSecureCacheStorage saveSecureCacheStorage;
+
+  LocalSaveCurrentAccount({this.saveSecureCacheStorage});
+
+  Future<void> save(AccountEntity account) async {
+    await saveSecureCacheStorage.saveSecure(key: 'token', value: account.token);
+  }
+}
+
+abstract class SaveSecureCacheStorage {
+  Future<void> saveSecure({@required String key, @required String value});
+}
+
+class SaveSecureCacheStorageSpy extends Mock implements SaveSecureCacheStorage {
+}
+
+void main() {
+  test('Should call CacheStorage with correct values', () async {
+    final cacheStorage = SaveSecureCacheStorageSpy();
+    final sut = LocalSaveCurrentAccount(saveSecureCacheStorage: cacheStorage);
+    final account = AccountEntity(faker.guid.guid());
+
+    await sut.save(account);
+
+    verify(cacheStorage.saveSecure(key: 'token', value: account.token));
+  });
+}
